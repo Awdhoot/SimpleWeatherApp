c0270109f8d20454d08869080d4b5108125158bb
awadhoot.lele <awadhoot.lele@synerzip.com>
2018-02-21 19:17:07 +0530

    added asios dependency for making AJAX calls

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 const API_KEY = "b72bb7dbafe70a1c803d5d084e22d560";
+const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+// constants for action types
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+
+//action creator to make AJAX call to fetch weather data, returns an action with a type and optional payload
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  return {
+    type: FETCH_WEATHER
+  }
+}
+
