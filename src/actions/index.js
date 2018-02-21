62d3f1846977abb90d8127995ff59f3735b1d27d
awadhoot.lele <awadhoot.lele@synerzip.com>
2018-02-21 19:21:19 +0530

    made API call to fetch weather using axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 const API_KEY = "b72bb7dbafe70a1c803d5d084e22d560";
 const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 // constants for action types
@@ -7,8 +9,10 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 //action creator to make AJAX call to fetch weather data, returns an action with a type and optional payload
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
   return {
-    type: FETCH_WEATHER
+    type: FETCH_WEATHER,
+    payload: request
   }
 }
 
