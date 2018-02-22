bf680fe6c1e3dea7fff412e42ef2041e18236771
awadhoot.lele <awadhoot.lele@synerzip.com>
2018-02-22 18:41:43 +0530

    Added logic to reducer to update weather state

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,12 @@
-export default function (state = null, action) {
-  console.log('action received -> ', state, action);
-  return state;
+import {FETCH_WEATHER} from '../actions/index';
+export default function (state = [], action) {
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // ALWAYS return new instance of state, DO NOT mutate current state
+      return [action.payload.data, ...state];
+      // return state.concat([action.payload.data])
+      return state;
+    default:
+      return state;
+  }
 }
