317602f1088e9a357353ee5ba8b23df4d0aa0a51
awadhoot.lele <awadhoot.lele@synerzip.com>
2018-02-22 17:48:35 +0530

    Connected searchBar submit event with fetchWeather API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = "b72bb7dbafe70a1c803d5d084e22d560";
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&appid=${API_KEY}`;
 // constants for action types
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
@@ -9,7 +9,7 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 //action creator to make AJAX call to fetch weather data, returns an action with a type and optional payload
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
-  const request = axios.get(url);
+  const request = axios.get(url, {});
   return {
     type: FETCH_WEATHER,
     payload: request
--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux'
 
-export default class SearchBar extends Component {
+// import action creator
+import {fetchWeather} from '../actions/index';
+
+class SearchBar extends Component {
   constructor(props) {
     super(props);
 
@@ -10,15 +15,18 @@ export default class SearchBar extends Component {
   onInputChange(event) {
     this.setState({term: event.target.value})
   }
+
   onFormSubmit(event) {
     // to prevent default form submission
     event.preventDefault();
+    this.props.fetchWeather(this.state.term);
+    this.setState({term: ''});
   }
 
   render() {
     return (
       <form onSubmit={this.onFormSubmit.bind(this)}
-        className="input-group mt-3">
+            className="input-group mt-3">
         <input className="form-control"
                value={this.state.term}
                placeholder=" Get a 5 day forecast in your favorite city "
@@ -30,3 +38,12 @@ export default class SearchBar extends Component {
     )
   }
 }
+
+// causes action creator to bind with middlewares and to dispatch an action to reducers
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({fetchWeather}, dispatch)
+}
+
+// 1st argument - mapStateToProps is not required here as this is controlled component.
+// Hence null
+export default connect(null, mapDispatchToProps)(SearchBar)
