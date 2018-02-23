a37e320b1136d72a5364b7546de4d29760dc8285
awadhoot.lele <awadhoot.lele@synerzip.com>
2018-02-23 13:14:30 +0530

    bit of error handling

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -2,8 +2,9 @@ import {FETCH_WEATHER} from '../actions/index';
 export default function (state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
+      console.log('action -> ', action);
       // ALWAYS return new instance of state, DO NOT mutate current state
-      return [action.payload.data, ...state];
+      return action.payload.data ? [action.payload.data, ...state] : state;
       // return state.concat([action.payload.data])
       return state;
     default:
